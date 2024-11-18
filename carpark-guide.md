86f854e9d730b06b3432203e40f8246a6a1d1411
20126321 <20126321@tafe.wa.edu.au>
2024-11-19 05:44:28 +0800

    feat(register method): core logic and exception raising

--- carpark-guide.md
+++ carpark-guide.md
@@ -207,11 +207,8 @@ You realize that you need a way to configure the car park system. You decide to 
 **Additional evidencing:**
 Ensure that you have completed the previous steps and created the appropriate tags. Confirm that the tags have been created by running `git tag` in the terminal and provide a screenshot of the output.
 
-```bash
-[student@workstation ipriot-car-park-prj]$ git tag
-s1
-s2
-```
+![tags](documentation/screenshots/tagss1s2.PNG)
+
 
 ### Relate the classes
 
--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,7 @@
+from display import Display
+from sensor import Sensor
+
+
 class CarPark:
     def __init__(self, location="Unknown", capacity=0, plates=None, sensors=None, displays=None):
         self.location = location
@@ -9,3 +13,11 @@ class CarPark:
     def __str__(self):
         return f"This car park is located at {self.location} and it's capacity is {self.capacity}."
 
+    def register(self, component):
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError("Object must be a Sensor or Display")
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+
+        elif isinstance(component, Display):
+            self.displys.append(component)
